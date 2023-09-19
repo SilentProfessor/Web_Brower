2f001e8bb658da9d67b2e68e1c67413193d7645d
SilentProfessor <142599405+SilentProfessor@users.noreply.github.com>
2023-09-20 01:58:07 +0530

    Python file
    run file

---
+++ Web_Brower.py
@@ -0,0 +1,11 @@
+print("                                                                   ✡   Rr Styamev Jayate️✡️")
+print()
+print("                       Help   Use   :  Enter Domain (example.com)   or  Enter Link (http/https://www.example.com)")
+print()
+
+import webbrowser
+link=input("Enter Web link  :  ")
+webbrowser.open(link)
+
+print()
+print("                                                         ⏳  Created  By Silent   Professor      ⏳  ")
